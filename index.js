550d73249fe4e640dea2a5bcb590e06253f71aba
TengLee888 <coffeealcohol@gmail.com>
2018-03-08 15:25:23 +0800

    repair token

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 
 const config = {
-  channelAccessToken: 'CoXKYe+ZuFR9j5tvXXN+zLwbpkW6LgXS1GeppXvrXI0C3M43hR643vZU0TpLTJeAXV2b/PevGJ6R2IwJo9IxBaU4AzsR3w1/uIMbsgXSKqC+LfWhQISxsG5Oh4oifXFkymKla88aFcfUZQ3/T8RrOgdB04t89/1O/w1cDnyilFU=
-',
-  channelSecret: 'b73b82bf14b98f6e89749521989429a9
+  channelAccessToken: 'CoXKYe+ZuFR9j5tvXXN+zLwbpkW6LgXS1GeppXvrXI0C3M43hR643vZU0TpLTJeAXV2b/PevGJ6R2IwJo9IxBaU4AzsR3w1/uIMbsgXSKqC+LfWhQISxsG5Oh4oifXFkymKla88aFcfUZQ3/T8RrOgdB04t89/1O/w1cDnyilFU=',
+  channelSecret: 'b73b82bf14b98f6e89749521989429a9'
 Issue'
 };
 
