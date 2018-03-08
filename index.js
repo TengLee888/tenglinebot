ad57552c8249160ee36e589a27f69274a85699a3
TengLee888 <coffeealcohol@gmail.com>
2018-03-08 15:14:35 +0800

    make it better

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const config = {
+  channelAccessToken: '比較長的那串',
+  channelSecret: '比較短的'
+};
+
+const app = express();
+app.post('/', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
+}
+
+app.listen(process.env.PORT || 8080, function() {
+  console.log("App now running on port");
+});
